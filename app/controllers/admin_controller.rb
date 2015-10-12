530e3fdbee312283723e02ddbabb2270186a7876
philmphoenix <jpholzmann@gmail.com>
2015-10-12 17:45:30 -0600

    WIP create rewards
    this is an awesome commit

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,11 +1,11 @@
 class AdminController < ApplicationController
   before_action :require_admin
 
-  def require_admin
-    if current_user && current_user.admin?
-      true
-    else
-      render file: "#{Rails.root}/public/404.html", layout: false, status: 404
+    def require_admin
+      if current_user && current_user.admin?
+        true
+      else
+        render file: "#{Rails.root}/public/404.html", layout: false, status: 404
+      end
     end
-  end
 end
